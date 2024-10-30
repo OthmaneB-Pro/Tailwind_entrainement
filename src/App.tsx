9056e3b41442f38202cb952d7c61a528017a7b95
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-10-30 19:10:14 +0100

    💄 Card final pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className=" bg-gray-300 h-screen">
       <div className="flex flex-col h-screen items-center ">
-        <h1 className="bg-[url('https://cdn.pixabay.com/photo/2023/10/31/16/38/ai-generated-8355516_640.png')] w-60 font-bold text-3xl p-1 rounded-md ml-6">
+        <h1 className="bg-[url('https://cdn.pixabay.com/photo/2023/10/31/16/38/ai-generated-8355516_640.png')] bg-cover bg-center w-60 h-20 font-bold text-3xl p-1 rounded-t-md ml-6">
           Pikachu
         </h1>
         <p className="w-60 p-4 mt-6 bg-white">
@@ -13,9 +13,11 @@ export default function App() {
           that store electricity. Pikachu's ears are long and come to a point,
           enhancing its cute appearance.
         </p>
-        <button className="bg-red-700 p-3 rounded-lg text-white font-bold">
-          Delete
-        </button>
+        <div className="bg-gray-500 w-60">
+          <button className="flex justify-center gap-2 mt-6 bg-red-700 p-3 rounded-lg text-white font-bold">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
